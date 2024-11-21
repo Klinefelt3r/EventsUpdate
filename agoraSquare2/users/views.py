3f9a3ca319b479b470c11ddd6a35467444d763e5
Klinefelt3r <cernak6juraj@gmail.com>
2024-11-21 12:26:00 +0100

    Initial commit

--- agoraSquare2/users/views.py
+++ agoraSquare2/users/views.py
@@ -1,6 +1,71 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.models import User
 from .models import Profile
+from .forms import CustomUserCreationForm
+def loginUser(request):
+    page = 'login'
 
+    if request.user.is_authenticated:
+        return redirect('profiles')
+
+    """
+    Handles user login request. If the request method is POST, it retrieves the username and password
+    from the request, attempts to authenticate the user, and logs them in if successful. If the user
+    does not exist or the credentials are incorrect, an error message is printed. Finally, it renders
+    the login/register template.
+    """
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        try:
+            user = Profile.objects.get(username=username)
+        except:
+            messages.error(request, "User does not exist")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('profiles')
+
+        else:
+            messages.error(request, "Username or password is incorrect")
+
+    return render(request, 'users/login_register.html')
+
+
+
+def logoutUser(request):
+        logout(request)
+        messages.error(request, "User was logged out")
+        return redirect('login')
+
+def registerUser(request):
+    page = 'register'
+    form = CustomUserCreationForm()
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('profiles')
+
+            messages.success(request, 'User account was created!')
+        else:
+            messages.error(request, 'Invalid form data')    
+
+            #login(request, user)
+            #return redirect('profiles')
+
+    context = {'page' : page, 'form': form}
+    return render(request, 'users/login_register.html', context)
 # Create your views here.
 def profiles(request):
     profiles = Profile.objects.all()
